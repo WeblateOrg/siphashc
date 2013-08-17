6032065e51216ab54acc392e4ac5a34a9423ae0f
elij <elij@wundrd.net>
2013-08-17 16:25:59 -0700

    add license file. cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,24 @@
 from distutils.core import setup, Extension
 
+long_description = """
+siphashc
+========
+
+python c-module for `siphash`_, based on `floodberry's version`_.
+
+Usage
+~~~~~
+
+.. code:: python
+
+    >>> from siphashc import siphash
+    >>> siphash('sixteencharstrng', 'i need a hash of this')
+    10796923698683394048L
+
+.. _siphash: https://131002.net/siphash/
+.. _floodberry's version: https://github.com/floodyberry/siphash
+""".strip()
+
 sipc = Extension(
     "siphashc",
     sources=['src/siphash/siphash.c', 'src/siphashc.c'],
@@ -11,4 +30,20 @@ setup(
     description='python module (in c) for siphash-2-4',
     url='http://github.com/cactus/siphashc',
     license="MIT",
-    ext_modules=[sipc])
+    ext_modules=[sipc],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    tests_require=[
+        'nose',
+        'unittest2',
+    ],
+    zip_safe=False,
+)
