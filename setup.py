ade8455bf916924e915c153959df94d9a0db8f42
Carlo Pires <carlopires@gmail.com>
2014-07-20 20:26:05 -0300

    Fix doc string for setup.py

--- setup.py
+++ setup.py
@@ -7,17 +7,22 @@ python c-module for `siphash`_, based on `floodberry's version`_.
 Usage
 ~~~~~
 
-.. code:: python2
+Python 2
+
+.. code:: python
 
     >>> from siphashc import siphash
     >>> siphash('sixteencharstrng', 'i need a hash of this')
     10796923698683394048L
 
-.. code:: python3
+Python 3
+
+.. code:: python
 
     >>> from siphashc import siphash
     >>> siphash('sixteencharstrng', 'i need a hash of this')
     10796923698683394048
+
 
 .. _siphash: https://131002.net/siphash/
 .. _floodberry's version: https://github.com/floodyberry/siphash
