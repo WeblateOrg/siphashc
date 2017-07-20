e000c064e41a35242ca6ede8ff9c7b4fb86ea11d
Michal Čihař <michal@cihar.com>
2017-07-20 16:17:21 +0200

    Remove not used siphashc.Error exception
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- siphashc.c
+++ siphashc.c
@@ -1,17 +1,17 @@
-/* 
+/*
  * Copyright (c) 2013 Eli Janssen
  * Copyright (c) 2014 Carlo Pires
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,17 +25,6 @@
 #include <string.h>
 #include <Python.h>
 #include "siphash/siphash.h"
-
-struct module_state {
-    PyObject *error;
-};
-
-#if PY_MAJOR_VERSION >= 3
-	#define GETSTATE(m) ((struct module_state*)PyModule_GetState(m))
-#else
-	#define GETSTATE(m) (&_state)
-	static struct module_state _state;
-#endif
 
 static PyObject *pysiphash(PyObject *self, PyObject *args) {
     const char *key = NULL;
@@ -78,16 +67,6 @@ static PyMethodDef siphashc_methods[] = {
 };
 
 #if PY_MAJOR_VERSION >= 3
-	static int siphashc_traverse(PyObject *m, visitproc visit, void *arg) {
-		Py_VISIT(GETSTATE(m)->error);
-		return 0;
-	}
-
-	static int siphashc_clear(PyObject *m) {
-		Py_CLEAR(GETSTATE(m)->error);
-		return 0;
-	}
-
 	static struct PyModuleDef moduledef = {
 		    PyModuleDef_HEAD_INIT,
 		    "siphashc",
@@ -95,8 +74,8 @@ static PyMethodDef siphashc_methods[] = {
 		    sizeof(struct module_state),
 		    siphashc_methods,
 		    NULL,
-		    siphashc_traverse,
-		    siphashc_clear,
+		    NULL,
+		    NULL,
 		    NULL
 	};
 
@@ -111,23 +90,15 @@ static PyMethodDef siphashc_methods[] = {
 	initsiphashc(void)
 #endif
 {
-	struct module_state *st;
-
+    PyObject *module;
 #if PY_MAJOR_VERSION >= 3
-    PyObject *module = PyModule_Create(&moduledef);
+    module = PyModule_Create(&moduledef);
 #else
-    PyObject *module = Py_InitModule("siphashc", siphashc_methods);
+    module = Py_InitModule("siphashc", siphashc_methods);
 #endif
 
     if (module == NULL)
         INITERROR;
-
-    st = GETSTATE(module);
-    st->error = PyErr_NewException("siphashc.Error", NULL, NULL);
-    if (st->error == NULL) {
-        Py_DECREF(module);
-        INITERROR;
-    }
 
 #if PY_MAJOR_VERSION >= 3
     return module;
