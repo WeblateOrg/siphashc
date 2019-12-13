ac8be1e7285dd0551c7d9dc73de0a84572873d5b
Michal Čihař <michal@cihar.com>
2019-12-13 10:03:50 +0100

    Black coding style
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- setup.py
+++ setup.py
@@ -5,37 +5,37 @@ import io
 import os.path
 from setuptools import setup, Extension
 
-with io.open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as readme:
+with io.open(
+    os.path.join(os.path.dirname(__file__), "README.rst"), encoding="utf-8"
+) as readme:
     LONG_DESCRIPTION = readme.read()
 
 setup(
-    name='siphashc',
-    version='1.3',
-    author='Michal Čihař',
-    author_email='michal@cihar.com',
-    description='Python module (in c) for siphash-2-4',
+    name="siphashc",
+    version="1.3",
+    author="Michal Čihař",
+    author_email="michal@cihar.com",
+    description="Python module (in c) for siphash-2-4",
     long_description=LONG_DESCRIPTION,
-    keywords='siphash siphash-2-4',
-    url='https://github.com/WeblateOrg/siphashc',
-    bugtrack_url='https://github.com/WeblateOrg/siphashc/issues',
+    keywords="siphash siphash-2-4",
+    url="https://github.com/WeblateOrg/siphashc",
+    bugtrack_url="https://github.com/WeblateOrg/siphashc/issues",
     license="ISC",
     ext_modules=[
         Extension(
-            name="siphashc",
-            sources=["siphashc.c", "siphash/siphash.c"],
-            language="c"
-        ),
+            name="siphashc", sources=["siphashc.c", "siphash/siphash.c"], language="c"
+        )
     ],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: ISC License',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: ISC License",
     ],
-    test_suite='test_siphashc',
+    test_suite="test_siphashc",
     zip_safe=True,
 )
