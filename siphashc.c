7f2b8e89772ab5b36b35a99891ea50ef9b1fbf03
Michal Čihař <michal@cihar.com>
2023-01-07 13:15:25 +0100

    Sync with WeblateOrg/meta

--- siphashc.c
+++ siphashc.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2013 Eli Janssen
  * Copyright (c) 2014 Carlo Pires
- * Copyright © 2017–2022 Michal Čihař
+ * Copyright © 2017–2023 Michal Čihař
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
