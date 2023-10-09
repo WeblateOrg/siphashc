259f4b14fe07b3fad45ca0eeee00fa9210849410
Michal Čihař <michal@cihar.com>
2023-10-09 10:29:30 +0200

    ci: modernize black, ruff, isort setup
    - drop isort, it is included in ruff
    - remove python version config, it is inferred from project.requires-python
    - apply more ruff rules

--- benchmark.py
+++ benchmark.py
@@ -12,6 +12,7 @@ print(timeit.timeit("siphash('0123456789ABCDEF', 'a')", "from siphashc import si
 print("Benchmark (long):")
 print(
     timeit.timeit(
-        "siphash('0123456789ABCDEF', 'a' * 1000)", "from siphashc import siphash"
-    )
+        "siphash('0123456789ABCDEF', 'a' * 1000)",
+        "from siphashc import siphash",
+    ),
 )
--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ from setuptools import Extension, setup
 setup(
     ext_modules=[
         Extension(
-            name="siphashc", sources=["siphashc.c", "siphash/siphash.c"], language="c"
-        )
+            name="siphashc",
+            sources=["siphashc.c", "siphash/siphash.c"],
+            language="c",
+        ),
     ],
 )
