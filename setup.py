71387a2d3298a1f0fa0f4cc5b949012f075067bc
Michal Čihař <michal@cihar.com>
2023-10-09 10:17:56 +0200

    chore: use pyproject.toml for setup metadata

--- setup.py
+++ setup.py
@@ -1,46 +1,13 @@
 #!/usr/bin/env python
 """Setup for the siphashc module."""
 
-import os.path
 
 from setuptools import Extension, setup
 
-with open(
-    os.path.join(os.path.dirname(__file__), "README.rst"), encoding="utf-8"
-) as readme:
-    LONG_DESCRIPTION = readme.read()
-
 setup(
-    name="siphashc",
-    version="2.1",
-    author="Michal Čihař",
-    author_email="michal@cihar.com",
-    description="Python module (in c) for siphash-2-4",
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/x-rst",
-    keywords="siphash siphash-2-4",
-    url="https://github.com/WeblateOrg/siphashc",
-    project_urls={
-        "Issue Tracker": "https://github.com/WeblateOrg/siphashc/issues",
-        "Source Code": "https://github.com/WeblateOrg/siphashc",
-        "Twitter": "https://twitter.com/WeblateOrg",
-        "Funding": "https://weblate.org/donate/",
-    },
-    license="ISC",
-    python_requires=">=3.8",
     ext_modules=[
         Extension(
             name="siphashc", sources=["siphashc.c", "siphash/siphash.c"], language="c"
         )
     ],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "License :: OSI Approved :: ISC License (ISCL)",
-    ],
-    zip_safe=True,
 )
