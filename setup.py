57b3973931bd57c964c941fe22365996fd23a26a
Michal Čihař <michal@cihar.com>
2025-10-08 14:23:19 +0200

    feat: build abi3 wheels

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 """Setup for the siphashc module."""
 
+import sysconfig
+
 from setuptools import Extension, setup
+
+define_macros = [("Py_LIMITED_API", "0x030A0000")]
+bdist_options = {"py_limited_api": "cp310"}
+
+# Free-threaded Python is not compatible with ABI3, see
+# https://github.com/python/cpython/issues/111506
+if sysconfig.get_config_var("Py_GIL_DISABLED"):
+    define_macros = []
+    bdist_options = {}
 
 setup(
     ext_modules=[
@@ -9,6 +20,9 @@ setup(
             name="siphashc",
             sources=["siphashc.c", "siphash/siphash.c"],
             language="c",
+            define_macros=define_macros,
+            py_limited_api=True,
         ),
     ],
+    options={"bdist_wheel": bdist_options},
 )
